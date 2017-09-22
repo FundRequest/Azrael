2f4c00058861d477de18ae7dbc58c8ae28d92b86
Quinten De Swaef <quintendeswaef@gmail.com>
2017-09-22 16:33:36 +0200

    starting the worker for azrael

---
+++ worker/src/main/java/io/fundrequest/azrael/worker/AzraelWorkerApp.java
@@ -0,0 +1,31 @@
+package io.fundrequest.azrael.worker;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+public class AzraelWorkerApp {
+
+    private static final Logger log = LoggerFactory.getLogger(AzraelWorkerApp.class);
+
+    public static void main(String[] args) throws UnknownHostException {
+        SpringApplication app = new SpringApplication(AzraelWorkerApp.class);
+        Environment env = app.run(args).getEnvironment();
+        log.info("\n----------------------------------------------------------\n\t"
+                        + "Application '{}' is running! Access URLs:\n\t"
+                        + "Local: \t\thttp://localhost:{}\n\t"
+                        + "External: \thttp://{}:{}\n\t" +
+                        "Profile(s): \t{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"),
+                env.getActiveProfiles());
+    }
+}
