9605dd0d36777c62998e773d2431836a987c7faa
Quinten De Swaef <quintendeswaef@gmail.com>
2018-05-18 11:52:02 +0200

    Fix csrf disabled

--- worker/src/main/java/io/fundrequest/azrael/worker/config/security/SecurityConfig.java
+++ worker/src/main/java/io/fundrequest/azrael/worker/config/security/SecurityConfig.java
@@ -34,6 +34,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .anyRequest().authenticated()
             .and()
             .httpBasic()
-            .authenticationEntryPoint(restAuthenticationEntryPoint);
+            .authenticationEntryPoint(restAuthenticationEntryPoint)
+            .and()
+            .csrf().disable();
     }
 }
